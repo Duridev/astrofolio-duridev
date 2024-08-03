0a5966e5cd2970175780fd66bb94bba1b265059a
duridev <duridev@outlook.com>
2024-08-03 02:15:32 -0400

    Navbar ready

---
+++ src/assets/data.js
@@ -0,0 +1,15 @@
+export const socials = [
+
+    {logo: 'bx bxl-linkedin-square'},
+    {logo: 'bx bxl-github'},
+    {logo: 'bx bxs-briefcase'}
+];
+
+
+export const info = [
+    {text: 'Perfil'},
+    {text: 'Experiencia'},
+    {text: 'Proyectos'},
+    {text: 'Estudios'},
+    {text: 'Contacto'}
+]
